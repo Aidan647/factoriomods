61919a728ebb3048797898c3d2af1c2134da7903
Kirill <Traineratwot@users.noreply.github.com>
2018-12-29 18:54:37 +0300

    6

--- X100_assembler_4.3.5/migrations/X100_assembler_4.3.5.lua
+++ X100_assembler_4.3.5/migrations/X100_assembler_4.3.5.lua
@@ -10,11 +10,16 @@ for _, force in pairs(game.forces) do
 		force.recipes["loader_X100"].enabled = true
 		force.recipes["transport_ground_X100"].enabled = true
 		force.recipes["transport_ground_X100_chanal1"].enabled = true
+		force.recipes["X100_pipe-ground"].enabled = true
+		force.recipes["X100_pipe"].enabled = true
 	end
 	if force.technologies["X100-train"].researched then
 		force.recipes["X100-locomotive-1"].enabled = true
 		force.recipes["X100-locomotive-tender"].enabled = true
 		force.recipes["X100-wagon-1"].enabled = true
 		force.recipes["X100-wagon-2"].enabled = true
+	end
+	if force.technologies["X100_refinery"].researched then
+		force.recipes["X100_refinery"].enabled = true
 	end
 end
