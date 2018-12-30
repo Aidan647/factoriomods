b962688c8053e46dbb0fb8dcbecebb50d509d1d3
Kirill <Traineratwot@users.noreply.github.com>
2018-12-30 16:31:38 +0300

    7

--- Yuoki_0.6.74/prototypes/item/y_stacksizes.lua
+++
@@ -1,8 +0,0 @@
-
-if data.raw["item"]["stone"].stack_size < 51 then
-	data.raw["item"]["stone"].stack_size=100
-	data.raw["item"]["coal"].stack_size=100
-	data.raw["item"]["raw-wood"].stack_size=100
-	data.raw["item"]["iron-ore"].stack_size=100
-	data.raw["item"]["copper-ore"].stack_size=100
-end 
